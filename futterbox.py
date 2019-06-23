3d9a07b10870772f5ff1e64018b7895a4da208c5
andreaspohl <andreas.g.pohl@gmail.com>
2019-06-23 19:38:17 +0200

    feat: green life sign all 5 sec

--- futterbox.py
+++ futterbox.py
@@ -6,9 +6,11 @@ from time import sleep
 sense = SenseHat()
 sense.set_rotation(90)
 
+black = (0, 0, 0)
 red = (255, 0, 0)
 yellow = (50, 50, 0)
 green = (0, 100, 0)
+faintGreen = (0, 48, 0)
 blue = (0, 0, 50)
 
 while True:
@@ -20,10 +22,10 @@ while True:
         else:
             sense.show_letter('X', text_colour=red)
     except:
-            sense.show_letter('?', text_colour=yellow)
-    for x in range(100, 256):
-        sense.set_pixel(7, 0, (0, 0, x))
-        sleep(0.015)
-    for x in range(255, 100, -1):
-        sense.set_pixel(7, 0, (0, 0, x))
-        sleep(0.015)
+        sense.show_letter('?', text_colour=yellow)
+
+    # life sign
+    sense.set_pixel(7,0,black)
+    sleep(0.1)
+    sense.set_pixel(7,0,faintGreen)
+    sleep(5)
